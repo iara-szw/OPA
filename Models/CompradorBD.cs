531d792ba280197ff6d0d0828d766960b21c10c2
Iara <iszwarstein@gmail.com>
2025-11-26 21:36:03 -0300

    correcion del commit anterior

--- Models/CompradorBD.cs
+++ Models/CompradorBD.cs
@@ -2,7 +2,7 @@ using Microsoft.Data.SqlClient;
 using Dapper;
 static class CompradorBD{
  
-    public static string connectionString = @"Server=localhost\SQLEXPRESS01; DataBase=OPA; Integrated Security=True; TrustServerCertificate=True;";
+    public static string connectionString = @"Server=localhost; DataBase=OPA; Integrated Security=True; TrustServerCertificate=True;";
     static public  void agregarComprador(Comprador usu){
 
         string query = "INSERT INTO Comprador (Usuario, Nombre, Apellido, Contraseña,Telefono,Mail,Genero,esVendedor) VALUES (@pUsuario, @pNombre, @pApellido, @pContraseña, @pTelefono, @pMail, @pGenero,@pVendedor)";
