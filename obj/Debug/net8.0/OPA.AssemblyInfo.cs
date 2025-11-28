7280427a4a4b52af4d52cc50b8cd937cdd26e62c
MAXIMILIANO BERNARDONI <49432830@ort>
2025-11-28 15:04:38 -0300

    Index arreglado

--- obj/Debug/net8.0/OPA.AssemblyInfo.cs
+++ obj/Debug/net8.0/OPA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OPA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e9b6f00bd7a88f4cd82819a06e9da92911b25f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb33064ec91d4a41e65c339d62fa3aca8579ec1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OPA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OPA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
